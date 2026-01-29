7c7b1fde93559018a72b24a8366beb2ad43ad9de
Kaveh Goodarzi <kaveh4.voll@gmail.com>
2026-01-29 18:07:20 +0330

    Add the first file

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Coin struct {
+	Bitcoin struct {
+		USD float64 `json:"usd"`
+	} `json:"bitcoin"`
+}
+
+func main() {
+	resp, err := http.Get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+	if err != nil {
+		fmt.Println("Error getting data")
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading data")
+	}
+
+	var humanData Coin
+	if err := json.Unmarshal(data, &humanData); err != nil {
+		fmt.Println("Error unmarshaling json data")
+	}
+	fmt.Println(humanData)
+	fmt.Printf("Bitcoin cost: %.2f\n", humanData.Bitcoin.USD)
+}
