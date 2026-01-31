9a86cd8c9fac6c86d97c16600731baf8c8170583
Kaveh Goodarzi <kaveh4.voll@gmail.com>
2026-01-31 15:33:36 +0330

    Add ETH coin

--- main.go
+++ main.go
@@ -7,30 +7,66 @@ import (
 	"net/http"
 )
 
-type Coin struct {
-	Bitcoin struct {
-		USD float64 `json:"usd"`
-	} `json:"bitcoin"`
+type Coin interface {
+	GetPrice() Coins
 }
 
-func main() {
+type Bitcoin struct {
+	USD float64 `json:"usd"`
+}
+
+type Coins struct {
+	BTC Bitcoint
+	ETH Ethereum
+}
+
+func (btc *Bitcoin) GetPrice() Coins {
 	resp, err := http.Get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
 	if err != nil {
-		fmt.Println("Error getting data")
+		fmt.Println("Error getting BTC data")
 	}
 	defer resp.Body.Close()
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Error reading data")
+		fmt.Println("Error reading BTC data")
 	}
 
-	var humanData Coin
-	if err := json.Unmarshal(data, &humanData); err != nil {
-		fmt.Println("Error unmarshaling json data")
+	var finallyData Coins
+	if err := json.Unmarshal(data, &finallyData); err != nil {
+		fmt.Println("Error unmarshaling BTC data")
 	}
-	fmt.Println(humanData)
-	fmt.Printf("Bitcoin cost: %.2f\n", humanData.Bitcoin.USD)
+	
+	return finallyData
+
+}
+
+type Ethereum struct {
+	USD float64 `json:"usd"`
+}
+
+func (eth *Ethereum) GetPrice() Coins {
+	resp, err := http.Get("https://api.coingeko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd")
+	if err != nil {
+		fmt.Println("Error getting ETH data")
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading ETH data")
+	}
+
+	var finallyData Coins
+	
+	if err := json.Unmarshal(data, &finallyData); err != nil {
+		fmt.Println("Error unmarshaling ETH data")
+	}
+
+	return finallyData
+
+func main() {
+
 }
 
 // Web-Server Will Add Here
